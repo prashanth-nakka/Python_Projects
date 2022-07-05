f3d94e6307b1488c937e24a767018f70b08ecf18
Prashanth <prashanth01997@gmail.com>
2022-07-05 21:26:19 +0530

    sending_mail

---
+++ Send_Email/main.py
@@ -0,0 +1,20 @@
+import smtplib as sm
+
+ob = sm.SMTP('smtp.gamil.com', 535)  # port of gmail = 587
+
+ob.ehlo()
+ob.starttls()
+ob.login('prashanth041997@gmail.com', 'Tooni@1997')
+subject = "Test Python"
+body = "I love Python"
+msg = "subject: {}\n\n{}".format(subject, body)
+print(msg)
+listadd = ['prashanth041997@gmail.com', 'prasanthnakka97@gmail.com']
+
+# To Send mail to receivers
+ob.sendmail('prashanth041997@gmail.com', listadd, msg)
+print("Mail Sent Successfully!")
+
+# To Close Server
+
+ob.quit()
